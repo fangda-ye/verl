75c843e7d1705c38711c33b3c9f3ccc0f2e22707
Fangda Ye <fangda.ye.cs@gmail.com>
2025-11-18 23:51:26 +0800

    Update run_pro_grpo.sh

--- examples/proactive/run_pro_grpo.sh
+++ examples/proactive/run_pro_grpo.sh
@@ -1,6 +1,6 @@
 python -m verl.trainer.main_ppo \
-    --config-path your/config/path \
-    --config-name your_config \
-    ++reward_model.reward_manager=group_aware \
-    ++custom_reward_function.path=/path/to/my_reward.py \
-    ++custom_reward_function.name=adaptive_reward
+    --config-path examples/proactive \
+    --config-name config_with_group_aware_reward \
+    # ++reward_model.reward_manager=group_aware \
+    # ++custom_reward_function.path=/path/to/my_reward.py \
+    # ++custom_reward_function.name=adaptive_reward
